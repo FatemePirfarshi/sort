3505e3a876f5656b7525ef0d4ce61f615019a177
fatemeh-pirfarsh <fatemeh.pf73@gmail.com>
2020-07-18 18:05:21 +0430

    Adding first file

---
+++ BubbleSort.java
@@ -0,0 +1,23 @@
+public class BubbleSort {
+
+
+    public void sort(int[] array){
+        int n = array.length;
+        boolean flag;
+        for (int i = 0; i < n; i++) {
+            flag = false;
+            for (int j = 0; j < n-i-1; j++) {
+
+                if(array[j+1] < array[j]){
+                    int next = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = next;
+                    flag = true;
+                }
+            }
+
+            if(!flag)
+                break;
+        }
+    }
+}
